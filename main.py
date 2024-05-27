45ee57c17635aec6085add16278c4dd5a2f092c4
Maxime Malette <132301458+madmax9845@users.noreply.github.com>
2024-05-27 17:33:01 -0400

    Pompe fonctionne avec l'humidité de la terre

--- main.py
+++ main.py
@@ -28,19 +28,52 @@ button.direction = digitalio.Direction.INPUT
 # Mise en place des objets de la carte Arduino
 i2c = board.I2C()
 dht = adafruit_dht.DHT11(board.IO8)
-pot = analogio.AnalogIn(board.A0)
+dht2 = analogio.AnalogIn(board.A0)
 led = digitalio.DigitalInOut(board.IO11)
 led.direction = digitalio.Direction.OUTPUT
 
 ecran = projet2.ecran()
 timerArrosage = 0
+timer = 0
+last_time_recolte = 0
+tableauHum = []
+total = 0
+hum_moyenne = 0
 while True:
 
+    # Gestion de la moyenne de l'humidité de la terre
+    if(time.monotonic() - last_time_recolte > 1):
+
+        last_time_recolte = time.monotonic()
+        humidite = dht2.value
+     
+        if len(tableauHum) < 120:
+            tableauHum.append(humidite)
+            total += humidite
+        else:
+            total -= tableauHum.pop(0)
+            tableauHum.append(humidite)
+            total += humidite
+
+        hum_moyenne = total/len(tableauHum)
+        print(hum_moyenne)
+
+    #Activation de la pompe avec le bouton 
     if(button.value == True and pump.value == False):
         print(pump.value)
         pump.value = True
         timerArrosage = time.monotonic()
     
+    #La pompe arrose pendant 30 secondes
     if(time.monotonic()-timerArrosage >30):
-        print(button.value)
         pump.value = False
+
+    #Activation de la pompe avec l'humidité de la terre
+    if(hum_moyenne >= 17000):
+        pump.value = True
+        timerArrosage = time.monotonic()
+    
+        # 17098 devrrait être arrosé
+        # 16800 assez arrosé
+
+    
